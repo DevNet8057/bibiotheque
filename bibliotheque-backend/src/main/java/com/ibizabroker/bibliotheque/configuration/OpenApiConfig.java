0e7147ab588f9f69b0c4cf99971eda4204113b74
NJEUTCHOU KOUEKOUA BENILDE <devnet8057@gmail.com>
2026-09-15 09:10:37 +0100

    feat: ajouter l'authentification JWT dans Swagger

--- bibliotheque-backend/src/main/java/com/ibizabroker/bibliotheque/configuration/OpenApiConfig.java
+++ bibliotheque-backend/src/main/java/com/ibizabroker/bibliotheque/configuration/OpenApiConfig.java
@@ -1,12 +1,17 @@
 package com.ibizabroker.bibliotheque.configuration;
 
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class OpenApiConfig {
+
+    private static final String BEARER_AUTH = "bearerAuth";
 
     @Bean
     public OpenAPI bibliothequeOpenAPI() {
@@ -14,6 +19,13 @@ public class OpenApiConfig {
                 .info(new Info()
                         .title("API Bibliotheque")
                         .description("API REST de gestion de bibliotheque : livres, utilisateurs et emprunts")
-                        .version("1.0"));
+                        .version("1.0"))
+                .components(new Components().addSecuritySchemes(BEARER_AUTH,
+                        new SecurityScheme()
+                                .type(SecurityScheme.Type.HTTP)
+                                .scheme("bearer")
+                                .bearerFormat("JWT")
+                                .description("Collez uniquement le JWT obtenu avec POST /authenticate.")))
+                .addSecurityItem(new SecurityRequirement().addList(BEARER_AUTH));
     }
 }
--- bibliotheque-backend/src/main/java/com/ibizabroker/bibliotheque/controller/JwtController.java
+++ bibliotheque-backend/src/main/java/com/ibizabroker/bibliotheque/controller/JwtController.java
@@ -3,6 +3,8 @@ package com.ibizabroker.bibliotheque.controller;
 import com.ibizabroker.bibliotheque.entity.JwtRequest;
 import com.ibizabroker.bibliotheque.entity.JwtResponse;
 import com.ibizabroker.bibliotheque.service.JwtService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.security.SecurityRequirements;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -15,6 +17,8 @@ public class JwtController {
     private JwtService jwtService;
 
     @PostMapping("/authenticate")
+    @Operation(summary = "Obtenir un jeton JWT", description = "Saisissez l'identifiant et le mot de passe, puis copiez jwtToken dans Authorize.")
+    @SecurityRequirements
     public JwtResponse createJwtToken(@RequestBody JwtRequest jwtRequest) throws Exception {
         return jwtService.createJwtToken(jwtRequest);
     }
